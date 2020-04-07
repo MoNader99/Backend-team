4c0429fcbd3e021c9ec39a7efd8e2b0c8f091dfc
Aya Elsackaan <ayaelsackaan.1999@gmail.com>
2020-04-07 03:25:39 +0200

    demo artists

--- demo.js
+++ demo.js
@@ -19,6 +19,14 @@ var imgPath1 = "default.png";   //default picture of playlist
 var imgPath2 = "Billie-Eilish.png";
 var imgPath3 = "Adele.png";
 var imgPath4 = "defaultuser.png";
+var imgPath5 = "AmrDiab.png";
+var imgPath6 = "eminem.png";
+var imgPath7 = "sia.png";
+var imgPath8 = "miley.png";
+var imgPath9 = "JenGr.png";
+var imgPath10 = "ed.png";
+var imgPath11 = "sel.png";
+var imgPath12 = "coldplay.png";
 
 
 
@@ -51,13 +59,13 @@ artist1.save().then((res)=>{
 });
 
 
-//CREATING A third ARTIST
+//CREATING A second ARTIST
 
 var artist2= new artist({
     email:"be12@hotmail.com",
     password:"$2b$10$sqP.uu/YJzYg0vErxw24TeMe8eeUzPtWCrSST8gGn9wMxYNQxqGNS",
     artistName:"Billie Eilish",
-    about:`Billie Eilish is an American singer-songwriter who first shot to prominence when she uploaded her breakout hit 
+    about:`Billie Eilish is an American singer-songwriter who first shot to prominence when she uploaded her breakout hit
     "Ocean Eyes" to SoundCloud in 2015. ... She worked with her brother, Finneas O'Connell, to record "Ocean Eyes,"
      a song O'Connell had initially written for his band`,
     genres: ["sha3by", "R&B"],
@@ -94,8 +102,143 @@ artist3.save().then((res)=>{
 
 
 
+var artist4= new artist({
+    email:"amrdiab@abc.com",
+    password:"2020",
+    artistName:"AmrDiab",
+    about:`Amr Diabis an Egyptian singer, composer and former actor. He has established himself as an acclaimed recording artist and author in most Mediterranean countries. According to a research by Michael Frishkopf, he has created his style termed as "Mediterranean Music", a blend of Western and Egyptian rhythms. By 1992, he became the first Egyptian and Middle Eastern artist to start making high-tech music videos.`,
+    genres:["pop","R&B"],
+    rating: 4,
+    imagePath:imgPath5,
+    gender:"M",
+    birthDate:"1988-05-05"
+});
+
+//SAVING AND RETURNING ID OF THE NEW ARTIST
+artist4.save().then((res)=>{
+    console.log(res._id);
+},(err)=>{
+    console.log(err);
+});
 
 
+
+var artist5= new artist({
+    email:"sia@abc.com",
+    password:"2020",
+    artistName:"Sia",
+    about:" is an Australian singer, songwriter, voice actress and music video director. She started her career as a singer in the acid jazz band Crisp in the mid-1990s in Adelaide. In 1997, when Crisp disbanded, she released her debut studio album titled OnlySee in Australia. She moved to London, England, and provided lead vocals for the British duo Zero 7. In 2000, Sia released her second studio album, Healing Is Difficult, and her third studio album, Colour the Small One, in 2004, but all of these struggled to connect with a mainstream audience.",
+    genres:["pop","R&B"],
+    rating: 4,
+    imagePath:imgPath5,
+    gender:"F",
+    birthDate:"1988-05-05"
+});
+
+//SAVING AND RETURNING ID OF THE NEW ARTIST
+artist5.save().then((res)=>{
+    console.log(res._id);
+},(err)=>{
+    console.log(err);
+});
+
+
+
+var artist6= new artist({
+    email:"miley@abc.com",
+    password:"2020",
+    artistName:"Miley Cyrus",
+    about:"is an American singer, songwriter, and actress. Her music has spanned a range of styles, including pop, country pop, and hip hop. Cyrus' personal life, public image, and performances have often sparked controversy and received widespread media coverage.",
+    genres:["pop","R&B"],
+    rating: 4,
+    imagePath:imgPath5,
+    gender:"F",
+    birthDate:"1988-05-05"
+});
+
+//SAVING AND RETURNING ID OF THE NEW ARTIST
+artist6.save().then((res)=>{
+    console.log(res._id);
+},(err)=>{
+    console.log(err);
+});
+
+var artist7= new artist({
+    email:"jen@abc.com",
+    password:"2020",
+    artistName:"Jennifer Grout",
+    about:"Jennifer Grout was born in Mesa (Arizona, US) and studied in McGill University in Canada. She is the daughter of a pianist and a violinist and began studying music at 5. In 2012 and after her summer trip to Morocco, Jennifer Grout got interested in Arabic and Amazigh culture and music. She competed in Arabs Got Talent and was finalist in 2013",
+    genres:["pop","R&B"],
+    rating: 4,
+    imagePath:imgPath5,
+    gender:"F",
+    birthDate:"1988-05-05"
+});
+
+//SAVING AND RETURNING ID OF THE NEW ARTIST
+artist7.save().then((res)=>{
+    console.log(res._id);
+},(err)=>{
+    console.log(err);
+});
+
+var artist8= new artist({
+    email:"edsh@abc.com",
+    password:"2020",
+    artistName:"Ed Sheeran",
+    about:"Ed Sheeran may be the quintessential pop star of the 2010s: a singer/songwriter who seems to acknowledge no boundaries between styles or eras, creating a sound that's idiosyncratic and personal. Sheeran borrows from any style that crosses his path -- elements of folk, hip-hop, pop, dance, soul, and rock can be heard in his big hits The A Team,Sing,Thinking Out Loud, and Shape of You -- which gives him a broad appeal among different demographics, helping to elevate him to international stardom not long after the release of his 2011 debut, +.",
+    genres:["pop","R&B"],
+    rating: 4,
+    imagePath:imgPath5,
+    gender:"M",
+    birthDate:"1988-05-05"
+});
+
+//SAVING AND RETURNING ID OF THE NEW ARTIST
+artist8.save().then((res)=>{
+    console.log(res._id);
+},(err)=>{
+    console.log(err);
+});
+
+var artist9= new artist({
+    email:"Sel@abc.com",
+    password:"2020",
+    artistName:"Selena Gomez",
+    about:"Selena Gomez is a multi-talented performer with featured roles on the screen, both small and big, and a musical career that reached the summit of the charts from the very beginning. Her group Selena Gomez & the Scene issued a trio of breezy modern pop albums from 2009 to 2011 that each reached the Top Ten. After going solo, she scored a pair of number ones with the more adult-oriented Stars Dance (2013) and Revival (2015), then branched out into new sounds on collaborations with Kygo (It Ain't Me) and Marshmello (Wolves). She hit the top of the singles chart for the first time with (Lose You to Love Me) (2019), a soul-searching ballad that delved into Gomez's real-life relationships and marked a new openness in her lyrical approach. The single's subsequent parent release, Rare (2020), became her third straight number one album.",
+    genres:["pop","R&B"],
+    rating: 4,
+    imagePath:imgPath5,
+    gender:"F",
+    birthDate:"1988-05-05"
+});
+
+//SAVING AND RETURNING ID OF THE NEW ARTIST
+artist9.save().then((res)=>{
+    console.log(res._id);
+},(err)=>{
+    console.log(err);
+});
+
+
+var artist10= new artist({
+    email:"coldplay@abc.com",
+    password:"2020",
+    artistName:"Coldplay",
+    about:"Coldplay came together as a band in late 1997 when Guy Berryman, Jonny Buckland, Will Champion and Chris Martin met at University College, London.  The band have gone on to become one of the planet’s most popular acts, selling more than 75 million copies of their seven Number One albums, which have spawned a string of classic hits including Yellow, Clocks, Fix You, Paradise, Viva La Vida and A Sky Full Of Stars. They’ve also filled their trophy cabinet with almost every major music award there is (including seven Grammys and nine BRITs). ",
+    genres:["pop","R&B"],
+    rating: 4,
+    imagePath:imgPath5,
+    gender:"M",
+    birthDate:"1988-05-05"
+});
+
+//SAVING AND RETURNING ID OF THE NEW ARTIST
+artist10.save().then((res)=>{
+    console.log(res._id);
+},(err)=>{
+    console.log(err);
+});
 //////////////MONICA
 //////////////MONICA
 var track1=new track({
@@ -195,12 +338,12 @@ var track1=new track({
 //CREATING NEW USER INSTANCES AND SAVING THEM
 var user1= new User({
     email:"        ayaelsackaan.1999@gmail.com      ",
-    password:"$2b$10$tZ9A05CzdvX9AodV6Q/aZOt/8bIIJT78rN3Ax1txwfkY8MJujc4ZK",  //111 
+    password:"$2b$10$tZ9A05CzdvX9AodV6Q/aZOt/8bIIJT78rN3Ax1txwfkY8MJujc4ZK",  //111
     isActive:true,
     userName:"hamadaaa  ",
     gender:"M",
     birthDate: '1990-06-19',
-    likedTracks:[track1,track2,track4], 
+    likedTracks:[track1,track2,track4],
     likedAlbums:[album1],
 
 });
@@ -218,7 +361,7 @@ var user2= new User({
     userName:"Mario1",
     gender:"F",
     birthDate: '1990-06-19',
-    
+
 });
 
 
