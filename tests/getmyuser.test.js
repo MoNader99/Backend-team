dc4eb527c5f53e174d26fc011a1fed272312947b
monica <monicaehab16@gmail.com>
2020-04-11 11:54:29 +0200

    commenting change password test

--- tests/getmyuser.test.js
+++ tests/getmyuser.test.js
@@ -54,74 +54,74 @@ describe('Get user profile /users/me', () => {
 
 });
 
-describe('Change Password /users/changePassword', () => {
+// describe('Change Password /users/changePassword', () => {
    
-    it('It should refuse inactive user', (done) => {
+//     it('It should refuse inactive user', (done) => {
 
-        var testuser = new User({
-            email: "nadamahmoudabdelfatah@gmail.com",
-            password: "$2b$10$6/4iKym5yIP0j0lItY59G.VCwDod/S2QXuwEAVWZqutavXsQlivs.",
-            userName: "Nada",
-            gender: "F",
-            birthDate: '1990-06-19',
-            isActive: true
-        });
+//         var testuser = new User({
+//             email: "nadamahmoudabdelfatah@gmail.com",
+//             password: "$2b$10$6/4iKym5yIP0j0lItY59G.VCwDod/S2QXuwEAVWZqutavXsQlivs.",
+//             userName: "Nada",
+//             gender: "F",
+//             birthDate: '1990-06-19',
+//             isActive: true
+//         });
 
 
-        testuser.save().then((res) => {
-            testuser.generateAuthToken().then((testToken)=>{
-            request(app)
-                .post('/users/changePassword')
-                .set('x-auth', testToken)
-                .send({oldPassword:"222", newPassword:"111",})
-                .expect(200)
-                .end((err, res) => {
-                    if (err) {
-                        done(err)
-                    }
-                    User.findOneAndRemove({ _id: testuser._id }, function (err) {
-                        if (!err) {
+//         testuser.save().then((res) => {
+//             testuser.generateAuthToken().then((testToken)=>{
+//             request(app)
+//                 .post('/users/changePassword')
+//                 .set('x-auth', testToken)
+//                 .send({oldPassword:"222", newPassword:"111",})
+//                 .expect(200)
+//                 .end((err, res) => {
+//                     if (err) {
+//                         done(err)
+//                     }
+//                     User.findOneAndRemove({ _id: testuser._id }, function (err) {
+//                         if (!err) {
 
-                            done();
+//                             done();
 
-                        }
-                        else {
+//                         }
+//                         else {
                            
-                        }
-                    });
-                });
-        }, (err) => {
-            console.log(err);
-        });
+//                         }
+//                     });
+//                 });
+//         }, (err) => {
+//             console.log(err);
+//         });
 
-        })
+//         })
 
-    })
-
-
-
-   it('Passing empty token', (done) =>
-   {
-       var token = "";
-       request(app)
-       .put(`/users/changePassword`)
-       .set('x-auth',token)
-       .expect(401)
-       .end(done)
-   })
+//     })
 
 
-    it('Passing valid token but did not find an according user', (done) =>
-     {
-         var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTZkZTVmNGE2N2EwZGJjMDU4Y2I0MDYiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTg0MzAwMDEyfQ.bq9qS5Z7a992i0_MTOqfVxmPmjOObKT2YPh7oHKkQ64";
-         request(app)
-         .put(`/users/changePassword`)
-         .set('x-auth',token)
-         .expect(401)
-         .end(done)
-     })
+
+//    it('Passing empty token', (done) =>
+//    {
+//        var token = "";
+//        request(app)
+//        .put(`/users/changePassword`)
+//        .set('x-auth',token)
+//        .expect(401)
+//        .end(done)
+//    })
 
 
-    });
+//     it('Passing valid token but did not find an according user', (done) =>
+//      {
+//          var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTZkZTVmNGE2N2EwZGJjMDU4Y2I0MDYiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTg0MzAwMDEyfQ.bq9qS5Z7a992i0_MTOqfVxmPmjOObKT2YPh7oHKkQ64";
+//          request(app)
+//          .put(`/users/changePassword`)
+//          .set('x-auth',token)
+//          .expect(401)
+//          .end(done)
+//      })
+
+
+//     });
 
 
