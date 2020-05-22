979e889acc46c1ec5e35eb95c676362b8e8d3c39
monica <monicaehab16@gmail.com>
2020-05-23 01:13:08 +0200

    unlike and like playlist test t3dil

--- tests/likeUnlikePlaylist.test.js
+++ tests/likeUnlikePlaylist.test.js
@@ -11,66 +11,6 @@ var id2=1;
 
 
 
-
-// describe('/playlists/:playlistId/like/unlike/me',()=>{
-
-
-//     afterEach(() => {
-//         playlist.findOneAndRemove({_id:ObjectID( playlistId)}).then((res)=>{console.log(" ")});
-//     playlist.findOneAndRemove({_id:ObjectID(id2)}).then((res)=>{console.log(" ")});
-        
-//        })
-
-// it('should give 200 if user liked or unliked a playlist',(done)=>{
-
-
-//     User.find().then((users)=>{
-//         var id= users[users.length-1]._id.toHexString();
-   
-//         users[users.length-1].generateAuthToken().then((token)=>{
-
-//             var playlist1 = new playlist({
-//                 userId:id,
-//                 playlistName:"sha3by",
-//                 privacy:false,
-            
-//             });
-
-//             playlist1.save().then((ress) => {
-
-//                  playlistId=playlist1._id.toHexString();
-//                 //console.log(playlistId);
-                
-//             request(app)
-//             .post(`/playlists/${playlistId}/like/unlike/me`)
-//             .set('x-auth',token)
-//             .expect(200)
-             
-//           })
-          
-//           .end(done)
-//          // playlist.deleteOne({_id:ObjectID( playlistId)});
-         
-//           })
-//          // console.log("hahahahha");
-  
-            
-
-
-
-//         })
-
-
-
-// })
-
-// })
-
-
-
-
-
-
 describe('/playlists/:playlistId/like/unlike/me',()=>{
 
     afterEach(() => {
@@ -96,22 +36,20 @@ it('should give 200 if user liked or unliked a playlist',(done)=>{
             playlist1.save().then((ress) => {
 
                  playlistId=playlist1._id.toHexString();
-                //console.log(playlistId);
+                
                 
             request(app)
             .post(`/playlists/${playlistId}/like/unlike/me`)
             .set('x-auth',token)
             .expect(200)
             
-        //     .expect((res)=>{
-        //      expect( res.body.message).toBe("playlist name changed successfully")  
-        //   })
+        
           
           .end(done)
-         // playlist.deleteOne({_id:ObjectID( playlistId)});
+         
          
           })
-         // console.log("hahahahha");
+         
   
             
 
@@ -144,7 +82,7 @@ it('should give 400 if user tried to like a private playlits',(done)=>{
            playlist1.save().then((ress) => {
 
                 playlistId=playlist1._id.toHexString();
-               //console.log(playlistId);
+               
                
            request(app)
            .post(`/playlists/${playlistId}/like/unlike/me`)
@@ -156,10 +94,10 @@ it('should give 400 if user tried to like a private playlits',(done)=>{
          })
          
          .end(done)
-        // playlist.deleteOne({_id:ObjectID( playlistId)});
+        
         
          })
-        // console.log("hahahahha");
+        
  
            
 
@@ -208,10 +146,10 @@ it('should give 404 if playlist not found',(done)=>{
          })
          
          .end(done)
-        // playlist.deleteOne({_id:ObjectID( playlistId)});
+    
         
          })
-        // console.log("hahahahha");
+        
  
            
 
@@ -242,7 +180,7 @@ it('should give 401 if user is not authenticated',(done)=>{
            playlist1.save().then((ress) => {
 
                 playlistId=playlist1._id.toHexString();
-               //console.log(playlistId);
+               
                
            request(app)
            .post(`/playlists/${playlistId}/like/unlike/me`)
@@ -254,10 +192,10 @@ it('should give 401 if user is not authenticated',(done)=>{
          })
          
          .end(done)
-        // playlist.deleteOne({_id:ObjectID( playlistId)});
+        
         
          })
-        // console.log("hahahahha");
+        
  
            
 
@@ -289,7 +227,7 @@ it('should give 404 if invalid id',(done)=>{
            playlist1.save().then((ress) => {
 
                 playlistId=playlist1._id.toHexString();
-               //console.log(playlistId);
+        
                
            request(app)
            .post(`/playlists/1234/like/unlike/me`)
@@ -301,10 +239,10 @@ it('should give 404 if invalid id',(done)=>{
          })
          
          .end(done)
-        // playlist.deleteOne({_id:ObjectID( playlistId)});
+    
         
          })
-        // console.log("hahahahha");
+        
  
            
 
@@ -329,262 +267,3 @@ it('should give 404 if invalid id',(done)=>{
 
 
 })
-
-// it('should give 401 if the user is not authenticated',(done)=>{
-    
-//     User.find().then((users)=>{
-//        var id= users[users.length-1]._id.toHexString();
-  
-//        users[users.length-1].generateAuthToken().then((token)=>{
-
-//            var playlist1 = new playlist({
-//                userId:id,
-//                playlistName:"sha3by",
-//                privacy:true,
-           
-//            });
-
-//            playlist1.save().then((ress) => {
-
-//                 playlistId=playlist1._id.toHexString();
-//                //console.log(playlistId);
-               
-//            request(app)
-//            .post(`/playlists/${playlistId}/edit`)
-//            .set('x-auth',token+"fdf")
-//            .send({	"playlistName":"favsx"})
-//            .expect(401)
-           
-//            .expect((res)=>{
-//             expect( res.body.message).toBe("authentication failed")  
-//          })
-         
-//          .end(done)
-//         // playlist.deleteOne({_id:ObjectID( playlistId)});
-        
-//          })
-//          //console.log("hahahahha");
- 
-           
-
-
-
-//        })
-
-
-
-// })
-
-// })
-
-
-// it('should give 403 if the user is not authorized',(done)=>{
-    
-//     User.find().then((users)=>{
-//        var id= users[users.length-1]._id.toHexString();
-  
-//        users[users.length-2].generateAuthToken().then((token)=>{
-
-//            var playlist1 = new playlist({
-//                userId:id,
-//                playlistName:"sha3by",
-//                privacy:true,
-           
-//            });
-
-//            playlist1.save().then((ress) => {
-
-//                 playlistId=playlist1._id.toHexString();
-//                //console.log(playlistId);
-               
-//            request(app)
-//            .post(`/playlists/${playlistId}/edit`)
-//            .set('x-auth',token)
-//            .send({	"playlistName":"favsx"})
-//            .expect(403)
-           
-//            .expect((res)=>{
-//             expect( res.body.message).toBe("you are not allowed to make this request")  
-//          })
-         
-//          .end(done)
-//         // playlist.deleteOne({_id:ObjectID( playlistId)});
-        
-//          })
-//          //console.log("hahahahha");
- 
-           
-
-
-
-//        })
-
-
-
-// })
-
-// })
-// it('should give 404 if invalid id',(done)=>{
-    
-//     User.find().then((users)=>{
-//        var id= users[users.length-1]._id.toHexString();
-  
-//        users[users.length-2].generateAuthToken().then((token)=>{
-
-//            var playlist1 = new playlist({
-//                userId:id,
-//                playlistName:"sha3by",
-//                privacy:true,
-           
-//            });
-
-//            playlist1.save().then((ress) => {
-
-//                 playlistId=playlist1._id.toHexString();
-//                //console.log(playlistId);
-               
-//            request(app)
-//            .post(`/playlists/37483/edit`)
-//            .set('x-auth',token)
-//            .send({	"playlistName":"favsx"})
-//            .expect(404)
-           
-//            .expect((res)=>{
-//             expect( res.body.message).toBe("Invalid id")  
-//          })
-         
-//          .end(done)
-//         // playlist.deleteOne({_id:ObjectID( playlistId)});
-        
-//          })
-//          //console.log("hahahahha");
- 
-           
-
-
-
-//        })
-
-
-
-// })
-
-// })
-
-
-
-// it('should give 404 if playlist not found',(done)=>{
-    
-//     User.find().then((users)=>{
-//        var id= users[users.length-1]._id.toHexString();
-  
-//        users[users.length-2].generateAuthToken().then((token)=>{
-
-//            var playlist1 = new playlist({
-//                userId:id,
-//                playlistName:"sha3by",
-//                privacy:true,
-           
-//            });
-
-//            playlist1.save().then((ress) => {
-
-//                 playlistId=playlist1._id.toHexString();
-//                //console.log(playlistId);
-               
-//            request(app)
-//            .post(`/playlists/${new ObjectID()}/edit`)
-//            .set('x-auth',token)
-//            .send({	"playlistName":"favsx"})
-//            .expect(404)
-           
-//            .expect((res)=>{
-//             expect( res.body.message).toBe("playlist not found")  
-//          })
-         
-//          .end(done)
-//         // playlist.deleteOne({_id:ObjectID( playlistId)});
-        
-//          })
-//          //console.log("hahahahha");
- 
-           
-
-
-
-//        })
-
-
-
-// })
-
-// })
-// it('should give 500 if dublicate names',(done)=>{
-
-//     User.find().then((users)=>{
-//        var id= users[users.length-1]._id.toHexString();
-  
-//        users[users.length-1].generateAuthToken().then((token)=>{
-
-        
-//         var playlist2 = new playlist({
-//             userId:id,
-//             playlistName:"favsx",
-//             privacy:true,
-        
-//         });
-
-//         playlist2.save();
-//  id2=playlist2._id;
-
-//            var playlist1 = new playlist({
-//                userId:id,
-//                playlistName:"sha3by",
-//                privacy:true,
-           
-//            });
-
-//            playlist1.save().then((ress) => {
-
-//                 playlistId=playlist1._id.toHexString();
-//                //console.log(playlistId);
-               
-//            request(app)
-//            .post(`/playlists/${playlistId}/edit`)
-//            .set('x-auth',token)
-//            .send({	"playlistName":"favsx"})
-//            .expect(500)
-           
-//            .expect((res)=>{
-//             expect( res.body.message).toBe("you already have a playlist with the same name")  
-//          })
-         
-//          .end(done)
-//         // playlist.deleteOne({_id:ObjectID( playlistId)});
-        
-//          })
-//          //console.log("hahahahha");
- 
-           
-         
-
-
-//        })
-
-
-
-// })
-
-// })
-
-
-
-
-
-
-
-
-
-
-
-// })
