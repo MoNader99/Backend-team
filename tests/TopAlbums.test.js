464ee629520237888e05c22bfd6093b17e025258
Aya Elsackaan <ayaelsackaan.1999@gmail.com>
2020-06-06 20:00:50 +0200

    top albums testing

---
+++ tests/TopAlbums.test.js
@@ -0,0 +1,67 @@
+const expect =require('expect');
+const request = require('supertest')
+//local imports
+const app=require('./../Index');
+var{album}= require("../models/album.js");
+var{User}= require("../models/users.js");
+
+describe('GET /albums/top', () => {
+
+  it('should get top albums ', (done) =>
+  {
+    User.find().then((users)=>
+    {
+      users[users.length-1].generateAuthToken().then((token)=>
+      {
+            request(app)
+            .get('/albums/top')
+            .set('x-auth',token)
+            .expect(200)
+            .end(done)
+
+    })
+   })
+ });
+
+ it('should refuse empty token ', (done) =>
+ {
+
+       track.find().then((tracks)=>
+       {
+           request(app)
+           .get('/albums/top')
+           .expect(403)
+           .end(done)
+       })
+   });
+
+   it('should refuse invalid token ', (done) =>
+   {
+
+         track.find().then((tracks)=>
+         {
+             request(app)
+             .get('/albums/top')
+             .set('x-auth',"invalid token")
+             .expect(401)
+             .end(done)
+         })
+     });
+
+    //  it('should return not found if the genre has no tracks ', (done) =>
+    //  {
+    //
+    //    User.find().then((users)=>
+    //    {
+    //      users[users.length-1].generateAuthToken().then((token)=>
+    //      {
+    //            request(app)
+    //            .get(`/tracks/?genre=invalid`)
+    //            .set('x-auth',token)
+    //            .expect(404)
+    //            .end(done)
+    //    })
+    //   })
+    // });
+
+})
